02d5a817ed0fbdeb03306d3fdf3e547dbe0431ea
RC-Guy123 <168445794+RC-Guy123@users.noreply.github.com>
2024-05-06 23:57:23 +0100

    Update task_e.c

--- task_e.c
+++ task_e.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
-int main(int argc, char *argv[]){
-	// Enter your code under here to read the filename from the command line
-	const char *filename = 0;
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	const char *filename = NULL;
+	printf("Enter the filename: ");
 	scanf("%s", &filename);
-	// Read number from file - do not edit
+
+	// Check if the correct number of arguments are passed
+	if (argc != 1)
+	{
+		printf("Error. Expected 1 filename.\n");
+		return 1; // Return non-zero to indicate an error
+	}
+
+	// Read the filename from the command line argument
+	filename = argv[1];
+
+	// Open the file
+	FILE *fptr = fopen(filename, "r");
+	if (fptr == NULL)
+	{
+		printf("Error. Could not open file.\n");
+		return 1; // Return non-zero to indicate an error
+	}
+
+	// Read number from file
 	double num;
-	FILE *fptr;
-	fptr = fopen(argv[1], "r");
-	fscanf(fptr, "%lf", &num);
+	if (fscanf(fptr, "%lf", &num) != 1)
+	{
+		printf("Error. Could not read number from file.\n");
+		fclose(fptr);
+		return 1; // Return non-zero to indicate an error
+	}
+
+	// Close the file
 	fclose(fptr);
-	// Enter your code from Lab D to print JSON output (with name, email, number1, number2) goes under here
+
+	// Print JSON output
 	const char *name = "Austin Jewell";
-    const char *email = "austin.jewell@student.manchester.ac.uk";
-    double number1 = num;
-    double number2 = num;
-    printf("{\"name\": \"%s\",\n", name);
-    printf(" \"email\": \"%s\",\n", email);
-    printf(" \"number1\": %.2f,\n", number1);
-    printf(" \"number2\": %.3e}\n", number2);
-	while (1) {
-        // Loop forever
-    }
-	// Do not edit below here
+	const char *email = "austin.jewell@student.manchester.ac.uk";
+	double number1 = num;
+	double number2 = num;
+	printf("{\"name\": \"%s\",\n", name);
+	printf(" \"email\": \"%s\",\n", email);
+	printf(" \"number1\": %.2f,\n", number1);
+	printf(" \"number2\": %.3e}\n", number2);
+
+	// Loop forever
+	while (1)
+	{
+	}
+
 	return 0;
 }
